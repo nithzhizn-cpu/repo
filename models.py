81e561765f5774a412b99cfba3faefb318432ce6
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-18 13:00:28 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, Text
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
 from sqlalchemy.sql import func
 from database import Base
-
 
 class User(Base):
     tablename = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True)
+    username = Column(String, unique=True, index=True)
     token = Column(String, unique=True, index=True)
+
     is_pro = Column(Boolean, default=False)
     pro_until = Column(DateTime, nullable=True)
 
@@ -19,8 +19,8 @@ class Message(Base):
     id = Column(Integer, primary_key=True, index=True)
     from_id = Column(Integer)
     to_id = Column(Integer)
-    ciphertext = Column(Text)
     iv = Column(Text)
+    ciphertext = Column(Text)
     created_at = Column(DateTime, default=func.now())
 
 
