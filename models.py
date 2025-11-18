10efff5e8ab893636df9010357bdc6d680a0fe24
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-18 13:22:56 +0200

    Update models.py

--- models.py
+++ models.py
@@ -2,12 +2,13 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, Text
 from sqlalchemy.sql import func
 from database import Base
 
+
 class User(Base):
     tablename = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    token = Column(String, unique=True, index=True)   # важливо!
+    token = Column(String, unique=True, index=True)
     is_pro = Column(Boolean, default=False)
     pro_until = Column(DateTime, nullable=True)
 
@@ -16,19 +17,19 @@ class Message(Base):
     tablename = "messages"
 
     id = Column(Integer, primary_key=True, index=True)
-    from_id = Column(Integer, index=True)
-    to_id = Column(Integer, index=True)
+    from_id = Column(Integer)
+    to_id = Column(Integer)
     ciphertext = Column(Text)
     iv = Column(Text)
-    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, default=func.now())
 
 
 class CallSignal(Base):
     tablename = "calls"
 
     id = Column(Integer, primary_key=True, index=True)
-    from_id = Column(Integer, index=True)
-    to_id = Column(Integer, index=True)
+    from_id = Column(Integer)
+    to_id = Column(Integer)
     type = Column(String)
     data = Column(Text)
-    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, default=func.now())
