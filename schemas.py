9c11c37f3fb10be511588f33d926a36870c3e9b2
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-18 10:04:14 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -2,13 +2,16 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
+# ---------- USERS ----------
 class UserOut(BaseModel):
     id: int
     username: str
     is_pro: bool = False
 
-    class Config:
-        orm_mode = True
+    model_config = {
+        "from_attributes": True
+    }
+
 
 class AccountStatus(BaseModel):
     id: int
@@ -16,28 +19,37 @@ class AccountStatus(BaseModel):
     is_pro: bool
     pro_until: Optional[datetime] = None
 
-    class Config:
-        orm_mode = True
+    model_config = {
+        "from_attributes": True
+    }
+
 
 class FakeUpgradeRequest(BaseModel):
     user_id: int
 
+
 class UserCreate(BaseModel):
-    username:str
+    username: str
 
+
+# ---------- MESSAGES ----------
 class MessageCreate(BaseModel):
-    to:int
-    iv:str
-    ciphertext:str
+    to: int
+    iv: str
+    ciphertext: str
 
+
+# ---------- CALLS ----------
 class CallOffer(BaseModel):
-    to:int
-    sdp:str
+    to: int
+    sdp: str
+
 
 class CallAnswer(BaseModel):
-    to:int
-    sdp:str
+    to: int
+    sdp: str
+
 
 class Candidate(BaseModel):
-    to:int
-    candidate:str
+    to: int
+    candidate: str
